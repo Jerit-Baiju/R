74dea40a6e242a8202ca4d4c31cf31a5511dd153
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-23 16:47:03 +0530

    vectors

---
+++ vectors.r
@@ -0,0 +1,29 @@
+"fruits"
+fruits <- c("apple","orange","banana")
+fruits
+
+"sequence"
+
+seq <- 1:10
+seq
+
+"vector length"
+paste("there are ",length(fruits),"fruits in the basket")
+
+"sorted fruits"
+sort(fruits)
+
+"access fruit 1"
+fruits[1]
+
+"access multiple fruits"
+fruits[c(1,3)]
+
+"change an item"
+fruits[3] <- "pear"
+fruits
+
+"sequence by 20"
+sequence <- seq(from=0, to=100, by=20)
+sequence
+
