afbccf1cd6ec2369bf02ba546dd9de5051bde2a1
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-23 15:26:51 +0530

    string operations

---
+++ string_oper.r
@@ -0,0 +1,16 @@
+str <- "Hello World !"
+
+# length
+"length"
+nchar(str)
+
+# check a string
+"check string"
+grep("H", str)
+grep("!", str)
+grep("W", str)
+
+name <- "Jerit"
+"paste "
+paste(str, name)
+
